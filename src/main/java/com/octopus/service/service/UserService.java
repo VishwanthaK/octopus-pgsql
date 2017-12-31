ceae8699ec00bd2c6604b971bd27259446c945ed
Vishwanatha K <VishwanthaK@users.noreply.github.com>
2017-12-31 13:17:57 +0530

    Update UserService.java

--- src/main/java/com/octopus/service/service/UserService.java
+++ src/main/java/com/octopus/service/service/UserService.java
@@ -1,19 +1,20 @@
 package com.octopus.service.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.octopus.service.domain.JwtUser;
 import com.octopus.service.domain.model.User;
+import com.octopus.service.domain.model.UserAddress;
 
 
 public interface UserService {
 	
 	User getUserByToken(String token);
 	
-	long getUserIdByToken(String token);
+	Long getUserIdByToken(String token);
 	
 	JwtUser getUserDetails(String token);
 	
-	String test(String value);
+	String addUserAddress(String token, UserAddress addressInput) throws JsonProcessingException;
 	
-	String returnSomeString();
-	
+	String getUserAddress(String token)throws JsonProcessingException;
 }
