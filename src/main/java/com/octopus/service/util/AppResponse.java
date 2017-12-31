dddc54cfe359f457e78d5a6ecd6c8948e22686f4
Vishwanatha K <VishwanthaK@users.noreply.github.com>
2017-12-31 13:32:03 +0530

    Add files via upload

--- src/main/java/com/octopus/service/util/AppResponse.java
+++ src/main/java/com/octopus/service/util/AppResponse.java
@@ -7,5 +7,10 @@ public class AppResponse {
 	}
 	
 	public static final String SUCCESSFUL_SIGNUP = "Your account is regisered successfully.";
+	public static final String SUCCESSFUL_ADDRESS_ADD = "Your address added successfully.";
+	public static final String SUCCESSFUL_ADDRESS_UPDATE = "Your address updated successfully.";
 	
+	
+	public static final String SUCCESSFUL_ITEM_ADD = "Item added successfully.";
+	public static final String SUCCESSFUL_ITEM_UPDATE = "Item updated successfully.";
 }
---
+++ src/main/java/com/octopus/service/util/ExceptionConstants.java
@@ -0,0 +1,7 @@
+package com.octopus.service.util;
+
+public final class ExceptionConstants {
+	
+	public static Integer OCTOPUS_PERMISSION_EXCEPTION_CODE = 999;
+
+}
